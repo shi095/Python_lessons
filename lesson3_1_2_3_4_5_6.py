70ea9d5e0a1b9ed189460c685e441c9c4f054edc
shilo <shi095@gmail.com>
2021-01-12 22:58:32 +0300

    [insert] Задание 1-6 Урок 3

---
+++ lesson3_1_2_3_4_5_6.py
@@ -0,0 +1,16 @@
+# Не смотрю решения Урока 3, потому что хочу сама разобраться.
+# Заполняю по мере выполнения
+# 5
+
+my_list = list(map(int,input('Введите строку чисел через пробел. Для остановки введите 100: ').split()))
+
+def sum_list(my_list):
+    return sum(my_list)
+
+i = 0
+while 100 not in my_list:
+    for i in range(len(my_list)):
+        elem = my_list[i]
+    print(f'Сумма введенных вами чисел равна {sum_list(my_list)}')
+    i += 1
+    my_list.extend(list(map(int, input('Введите строку чисел через пробел. Для остановки введите 100: ').split())))
