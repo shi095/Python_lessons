247efc289d7f1fa4ce31b35cb40076ecc7e1a5a9
shilo <shi095@gmail.com>
2021-01-14 16:24:42 +0300

    [insert] Задание 1-7 Урок 4

--- lesson4_1_2_3_4_5_6_7.py
+++ lesson4_1_2_3_4_5_6_7.py
@@ -1,1 +1,16 @@
 # Cоздана ветка для наполнения заданиями к уроку 4
+# 1
+# Через командную строку переходим в папку, в которой хранится скрипт
+# C:/Users/shilo/PycharmProjects/pythonProject_2020/venv/
+# формируем лист для расчета заработной платы сотрудника
+# python lesson4_1.py 8 20 200
+# ['lesson4_1.py', '8', '20', '200'] - лист
+# 360 - полученный результат
+
+from sys import argv
+print(argv)
+
+def salary_func(sal_hour,hour,bonus):
+    return int(sal_hour)*int(hour)+int(hour)
+
+print(salary_func(argv[1],argv[2],argv[3]))
