c00203e5b3cb10d9c44ea00c6bf9fb276f3494c4
shilo <shi095@gmail.com>
2021-01-18 23:51:06 +0300

    Добавила решение 2

--- lesson5_1_2_3_4_5_6_7.py
+++ lesson5_1_2_3_4_5_6_7.py
@@ -13,3 +13,23 @@ while message != "":
 with open("text.txt", "w") as file:
         for message in messages:
                 file.write(message)
+
+# 2
+
+from io import TextIOWrapper, BufferedWriter
+
+with open('text5_2.txt','r') as f: # вывожу на экран содержимое файла для пользователя
+    lines = f.read()
+    print(lines)
+
+with open('text5_2.txt', 'r') as f: # создаю лист из строк
+    list = f.read().splitlines()
+
+lines = len(list)
+
+print(f'Количество строк в файле: {lines}')
+
+i = 0
+while i < len(list):
+    print(f'В строке {i+1} количество слов: {len(list[i].split())} ')
+    i += 1
