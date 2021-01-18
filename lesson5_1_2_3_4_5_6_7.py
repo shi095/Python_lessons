8b1107812e1ff01a238162cb721df4dd5996d863
shilo <shi095@gmail.com>
2021-01-18 16:39:11 +0300

    Добавила решение 1

--- lesson5_1_2_3_4_5_6_7.py
+++ lesson5_1_2_3_4_5_6_7.py
@@ -1,1 +1,15 @@
 # создана ветка для выполнения заданий 1-7 Урока 5
+# 1
+from io import TextIOWrapper
+
+messages = list()
+message = 0
+while message != "":
+        message = input("Введите строку: ")
+        messages.append(message + "\n")
+        if message == "":break
+
+
+with open("text.txt", "w") as file:
+        for message in messages:
+                file.write(message)
