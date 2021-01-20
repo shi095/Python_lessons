6007a9d8c9e5f3869f037f86a11b343e3ae63fc7
shilo <shi095@gmail.com>
2021-01-20 16:59:10 +0300

    Добавила комментрий к 7ому заданию

--- lesson5_1_2_3_4_5_6_7.py
+++ lesson5_1_2_3_4_5_6_7.py
@@ -97,3 +97,6 @@ with open("text5_6.txt", encoding='utf-8') as lessons:
         data = line.replace('(', ' ').split()
         subjects[data[0][:-1]] = sum(int(i) for i in data if i.isdigit())
     print(f'Словарь: {subjects}')
+
+# 7
+Осталось сделать 7ое задание статус 20.01.21
