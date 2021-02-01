42defb3b2c92d9e78526feb9ce5ccae4b7175b6d
shilo <shi095@gmail.com>
2021-02-01 17:31:44 +0300

    Добавлено решение задачи 2 Урок 6

--- lesson6_1_2_3_4_5.py
+++ lesson6_1_2_3_4_5.py
@@ -19,3 +19,19 @@ class TrafficLight: # атрибут приватный
 
 TrafficLight = TrafficLight() # создали экземпляр
 TrafficLight.running() # вызвали метод класса
+
+# 2
+
+class Road:
+
+    def __init__(self,length,width):
+        self._length = length
+        self._width = width
+
+
+    def mass_of_asphalt(self, mass,thickness):
+        return self._length*self._width*mass*thickness
+
+r = Road(20,5000)
+result =r.mass_of_asphalt(25,5)
+print(f'Масса асфальта,необходимого для покрытия всего дорожного полотна:{result}кг.')
