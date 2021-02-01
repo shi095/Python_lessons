607e4a1232bcab895aed1bd4510dc908b334802e
shilo <shi095@gmail.com>
2021-02-01 16:26:19 +0300

    Добавлена ветка для заданий по Уроку 8

--- lesson6_1_2_3_4_5.py
+++ lesson6_1_2_3_4_5.py
@@ -1,1 +1,22 @@
-# Создана ветка для выполнения Урока 6
+# 1
+
+from time import sleep
+# вызываем модуль для задержки
+class TrafficLight: # атрибут приватный
+    __color = ['красный', 'желтый', 'зеленый']
+
+    def running(self): # метод класса
+        i = 0
+        while i < 3:
+            print(f'Светофор переключается на : {TrafficLight.__color[i]}')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(2)
+            elif i == 2:
+                sleep(5)
+            i += 1
+
+
+TrafficLight = TrafficLight() # создали экземпляр
+TrafficLight.running() # вызвали метод класса
