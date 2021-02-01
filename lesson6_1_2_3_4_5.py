1a994461fbd6ec47c5f39368f33fc0366a6a93ba
shilo <shi095@gmail.com>
2021-02-02 01:42:04 +0300

    Добавлено решение задачи 4 Урок 6

--- lesson6_1_2_3_4_5.py
+++ lesson6_1_2_3_4_5.py
@@ -35,3 +35,95 @@ class Road:
 r = Road(20,5000)
 result =r.mass_of_asphalt(25,5)
 print(f'Масса асфальта,необходимого для покрытия всего дорожного полотна:{result}кг.')
+
+# 4
+
+class Car:
+
+    def __init__(self,speed,color,name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return f'Машина марки {self.name} поехала.'
+
+    def stop(self):
+        return f'Машина марки {self.name}  остановилась.'
+
+    def turn_left(self):
+        return f'Машина марки {self.name} повернула налево'
+
+    def turn_right(self):
+        return f'Машина марки {self.name}  повернула направо.'
+
+    def show_speed(self):
+        return f'Скорость машины марки {self.name} составлет {self.speed}.'
+
+class TownCar(Car):
+    def __init__(self,speed,color,name, is_police):
+        super().__init__(speed,color,name, is_police)
+
+    def show_speed(self):
+        print(f'Скорость машины марки {self.name} составлет {self.speed}.')
+        max_speed = 60
+        if self.speed > max_speed:
+            return f'Скорость машины марки {self.name} выше {max_speed}. Сбавьте скорость'
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+class WorkCar(Car):
+    def __init__(self,speed,color,name, is_police):
+        super().__init__(speed,color,name, is_police)
+
+    def show_speed(self):
+        print(f'Скорость машины марки {self.name} составлет {self.speed}.')
+        max_speed = 40
+        if self.speed > max_speed:
+            return f'Скорость машины марки {self.name} выше {max_speed}. Сбавьте скорость'
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police == True:
+            return f'Машина марки {self.name} - полицейская машина.'
+        else:return f'Машина марки {self.name} - не полицейская машина.'
+
+
+
+KIA = TownCar(70,'желтый','KIA', False)
+AUDI = SportCar(150,'черный','AUDI', False)
+WV = WorkCar(50,'красный','WV', False)
+FORD = PoliceCar(100,'синий','FORD', True)
+
+print(KIA.go())
+print(KIA.stop())
+print(KIA.turn_left())
+print(KIA.turn_right())
+print(KIA.show_speed())
+
+
+print(AUDI.go())
+print(AUDI.stop())
+print(AUDI.turn_left())
+print(AUDI.turn_right())
+print(AUDI.show_speed())
+
+print(WV.go())
+print(WV.stop())
+print(WV.turn_left())
+print(WV.turn_right())
+print(WV.show_speed())
+
+
+print(FORD.go())
+print(FORD.stop())
+print(FORD.turn_left())
+print(FORD.turn_right())
+print(FORD.show_speed())
+print(FORD.police())
